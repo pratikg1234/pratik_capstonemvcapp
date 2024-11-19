49052885ce3aa4583fae97cce41c7eda852edf45
ram <dev2@gmail.com>
2024-11-19 20:01:50 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pratik_capstonemvcapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
